197d7ea8505700130ad42e05c66a48e93c0681bc
JoEs <jes053022@gmail.com>
2022-04-09 01:19:18 +0900

    가우스 조던 소거법을 활용한 3차 연립방정식 셰산기

--- src/연습문제/ExInverseMatrix_Flower2.java
+++ src/연습문제/ExInverseMatrix_Flower2.java
@@ -17,7 +17,7 @@ public class ExInverseMatrix_Flower2 {
 		System.out.println("방정식의 상수항들을 순서대로 입력해주세요: ");
 		for(int i = 1; i < 4; i++) {
 			String coefficient = scan.next();
-			coe.add(i*3+i-1, coefficient);
+			coe.add(i*4-1, coefficient);
 		}		
 
 		double [] coef = new double[12];
@@ -33,25 +33,28 @@ public class ExInverseMatrix_Flower2 {
 			int i;
 
 			for(i=0; i<4; i++) {
-				temp[i] = coef[i]/coef[0+count];
+				temp[i] = coef[i]/coef[count];
 			}
+			
 			for(i=4; i<8; i++) {
 				temp[i] = coef[i]-coef[4+count]*temp[i-4];
 			}
+			
 			for(i=8; i<12; i++) {
 				temp[i] = coef[i]-coef[8+count]*temp[i-8];
 			}
+			
 			for(i=0; i<12; i++) {
 				coef[(i+8)%12]=temp[i];
 			}
 			count++;
 		}
 		System.out.println();
-		System.out.printf("%2.2f\t%2.2f\t%2.2f\t%n", coef[0],coef[1],coef[2]);
-		System.out.printf("%2.2f\t%2.2f\t%2.2f\t%n", coef[4],coef[5],coef[6]);
-		System.out.printf("%2.2f\t%2.2f\t%2.2f\t%n", coef[8],coef[9],coef[10]);
+		System.out.printf("%2.1f\t%2.1f\t%2.1f\t%n", coef[0],coef[1],coef[2]);
+		System.out.printf("%2.1f\t%2.1f\t%2.1f\t%n", coef[4],coef[5],coef[6]);
+		System.out.printf("%2.1f\t%2.1f\t%2.1f\t%n", coef[8],coef[9],coef[10]);
 		System.out.println();
-		System.out.printf("x=%2.2f y=%2.2f z=%2.2f입니다.", coef[3], coef[7], coef[11]);
+		System.out.printf("x=%2.1f y=%2.1f z=%2.1f입니다.", coef[3], coef[7], coef[11]);
 		scan.close();
 	}
 }
