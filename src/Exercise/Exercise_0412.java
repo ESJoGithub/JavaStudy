38a4055061644d74c312b8bb027b26ee4176108f
JoEs <jes053022@gmail.com>
2022-04-10 12:28:38 +0900

    구구단 일부만 출력하기

---
+++ src/Exercise/Exercise_0412.java
@@ -0,0 +1,28 @@
+package Exercise;
+
+public class Exercise_0412 {
+
+	public static void main(String[] args) {
+		// 구구단 일부 출력하기
+
+		int k = 2;
+		TIMES: for(int i = 1; i <= 3; i++) {
+			for(int j = k; j <= 9; j++) {
+				System.out.print(j + "*" + i + "=" + i*j +"\t");
+				if (j ==4 || j == 7 || j == 9) {
+					System.out.println();
+					if(i < 3) {
+					continue TIMES;
+					}
+					else {
+						System.out.println();
+						k = j+1;
+						i = 0;
+						continue TIMES;
+					}
+				}
+			}
+		}
+	}
+}
+
