55884e825717ee23ff9ce5cbd537a63c40aaad87
JoEs <jes053022@gmail.com>
2022-04-12 19:37:12 +0900

    구간합 구하기

--- src/codingTest/Ex03_PrefixSum.java
+++ src/codingTest/Ex03_PrefixSum.java
@@ -10,24 +10,19 @@ public class Ex03_PrefixSum {
 		int n = 0, m = 0;
 		
 		System.out.println("합을 구하고자 하는 수의 개수와 횟수를 띄어쓰기로 구분하여 입력하세요(1~100,000): ");
-		n = scan.nextInt();
-		m = scan.nextInt();
-		int[] sum = new int[n+1];
-		int[] num = new int[n+1];
-		for(int i = 1; i < sum.length; i++) {
-			num[i] = n;
-			n--;
-			if(i == 0) {
-				sum[i] = num[i];
-			}
-			else {
-			sum[i] = sum[i-1] + num[i];
-			}
+		n = scan.nextInt();         // 합을 구하고자 하는 범위
+		m = scan.nextInt();         // 합을 구하고자 하는 구역 설정 횟수
+		int[] sum = new int[n+1];   // 누적 합의 배열
+		int[] num = new int[n+1];   // 설정한 범위부터 역정렬용 배열
+		for(int i = 1; i < sum.length; i++) {  //num[0]과 sum[0]은 비워놓고 채운다.
+			num[i] = n;                        
+			n--;		                       // num[1]= n num[2]=n-1...num[n]=1
+			sum[i] = sum[i-1] + num[i];        // sum[1]=0+n sum[2]=n+n-1...
 			System.out.print(num[i] + " ");
 		}
 		System.out.println();
 		
-		for(int k = 1; k <= m; k++) {
+		for(int k = 1; k <= m; k++) { // 입력값 m만큼 반복
 			System.out.println("합을 구할 구간을 띄어쓰기로 구분하여 입력하세요: ");
 			int i = scan.nextInt();
 			int j = scan.nextInt();
